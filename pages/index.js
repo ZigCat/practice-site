61cd005f27b001d7d39253b08d99e9d35eaabc92
ZigCat <zigcat517@gmail.com>
2022-06-10 16:19:53 +0600

    fixed: index.js compatibility with different scale

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Head from 'next/head';
+import Head from "next/head";
 import Cabinet from "../components/Cabinet";
 import Currency from "../components/Currency";
 import Select from "../components/Select";
@@ -11,44 +11,50 @@ const Main = () => {
       <Head>
         <title>Главная страница SID project</title>
       </Head>
-      <div className="mainpage-inner">
-        <div className="mainpage-frow">
-          <Slider />
-          <div className="mainpage-frow-pop">
-            <img src="/content/bestseller.png" alt="" />
+      <div className="container">
+        <div className="mainpage-inner">
+          <div className="mainpage-frow">
+            <Slider />
+            <div className="mainpage-frow-pop">
+              <img src="/content/bestseller.png" alt="" />
+            </div>
+            <div className="mainpage-frow-right">
+              <Cabinet />
+              <Currency />
+            </div>
           </div>
-          <div className="mainpage-frow-right">
-            <Cabinet />
-            <Currency />
-          </div>
-        </div>
-        <div className="mainpage-srow">
-          <div className="mainpage-srow-selects">
-            <Select
-              headName="Смартфоны"
-              options={["Iphone 13 PRO", "Samsung S22", "Xiaomi 11 Pro"]}
-              link="/smartphones"
-            />
-            <Select
-              headName="Бытовая техника"
-              options={["Gorenje NRK619FAS4", "Tefal FV-5697", "LG F-2M5HS6W"]}
-              link="/kitchen"
-            />
-            <Select
-              headName="Аксессуары"
-              options={[
-                "Зарядка Apple",
-                "Чехол IPhone 13 Pro",
-                "Apple Watch Nike",
-              ]}
-              link="/accessories"
-            />
-          </div>
-          <div className="mainpage-srow-pop">
-            <img src="/content/promo1.jpg" alt="" />
-            <img src="/content/promo2.png" alt="" />
-            <img src="/content/promo3.png" alt="" />
-            <img src="/content/promo4.png" alt="" />
+          <div className="mainpage-srow">
+            <div className="mainpage-srow-selects">
+              <Select
+                headName="Смартфоны"
+                options={["Iphone 13 PRO", "Samsung S22", "Xiaomi 11 Pro"]}
+                link="/smartphones"
+              />
+              <Select
+                headName="Бытовая техника"
+                options={[
+                  "Gorenje NRK619FAS4",
+                  "Tefal FV-5697",
+                  "LG F-2M5HS6W",
+                ]}
+                link="/kitchen"
+              />
+              <Select
+                headName="Аксессуары"
+                options={[
+                  "Зарядка Apple",
+                  "Чехол IPhone 13 Pro",
+                  "Apple Watch Nike",
+                ]}
+                link="/accessories"
+              />
+            </div>
+            <div className="mainpage-srow-pop">
+              <img src="/content/promo1.jpg" alt="" />
+              <img src="/content/promo2.png" alt="" />
+              <img src="/content/promo3.png" alt="" />
+              <img src="/content/promo4.png" alt="" />
+            </div>
           </div>
         </div>
       </div>
